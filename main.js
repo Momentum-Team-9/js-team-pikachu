074d9b15e3dc0c660fac0c4ee4d9c5a6aa840831
sarah-wines <sarah.lodico.wines@gmail.com>
2021-07-28 20:59:29 -0400

    add album art and flex-wrap

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const form = document.getElementById('searchContainer');
-let url = 'https://proxy-itunes-api.glitch.me/search?limit=10&term=';
+let url = 'https://proxy-itunes-api.glitch.me/search?limit=18&term=';
 
 form.addEventListener('submit', function (event) {
   event.preventDefault();
@@ -16,7 +16,10 @@ form.addEventListener('submit', function (event) {
 function musicCardToHtml(music) {
   // const preview = music.previewUrl
   return `<div class="card">
+
+  <img src= ${music.artworkUrl100} alt="Album art">
   <div class="artist">${music.artistName}</div>
+  
   <div class="song">${music.trackName}</div>
   <button type="button" class="play" data-preview-url=${music.previewUrl}>Play</button>
   </div>`;
@@ -29,7 +32,7 @@ function search(searchText) {
   })
     .then((response) => response.json())
     .then((data) => {
-      for (var i = 0; i < 6; i++) {
+      for (var i = 0; i < 18; i++) {
         document.querySelector('#container').innerHTML += musicCardToHtml(
           data.results[i]
         );
@@ -46,9 +49,9 @@ card.addEventListener('click', function (e) {
   e.preventDefault();
   if (e.target && e.target.matches('button.play')) {
     console.log(' was clicked!');
-    console.log(e.target.dataset.previewUrl)
+    console.log(e.target.dataset.previewUrl);
     // use that url as the src attribute on the audio player
-    const audioPlayer = document.querySelector("#player")
-    audioPlayer.src = e.target.dataset.previewUrl
+    const audioPlayer = document.querySelector('#player');
+    audioPlayer.src = e.target.dataset.previewUrl;
   }
 });
